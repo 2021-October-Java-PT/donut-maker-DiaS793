136ad13afae52dff97c9518b8dd5b1fffb81675e
DiaS793 <DiaScruggs793@outlook.com>
2022-01-13 14:07:21 -0500

    tests issues

--- DonutMaker.js
+++ DonutMaker.js
@@ -1,11 +1,37 @@
 class DonutMaker{
-    constructor(donutCount){
+    constructor(donutCount, autoClickerCount){
         this.donutCount=donutCount;
+        this.autoClickerCount=autoClickerCount;
+        // this.donutMulti=donutMulti;
     }
 
-    addDonut(){
-        this.dountCount += 1;
-    }
+    addDonut() {
+        this.donutCount+=1;
+      }
+
+      addAutoClickerCount(){
+          this.autoClickerCount +=0.1;
+      }
+    // addAutoClicker(){
+    //     this.autoClicker += 0.1;
+    // }
+
+    // takeAwayDonut(){
+    //     this.autoClicker += 0.1;
+    //     this.donutCount -= 0.1;
+    // }
+
+    // addDonutMulti(){
+    //     this.addDonutMulti+= 1;
+    // }
+
+    // multiplyByDonutMulti(){
+    //     Math.pow(1.2, DonutMultiplierCount)
+    // }
+
+    
+
+    
   
   
 }
--- donut-Maker.test.js
+++ donut-Maker.test.js
@@ -1,18 +1,63 @@
 import DonutMaker from "./DonutMaker";
 
-describe('DonuteMaker', ()=> {
+describe('DonutMaker', ()=> {
 
     test ('store Donut', () => {
         const underTest = new DonutMaker(0);
         expect(typeof underTest.donutCount).toMatch('number');
     });
 
-    test('add Donut', ()=> {
-        const underTest = new DonutMaker(0);
-        underTest.addDonut();
-        expect(underTest.donutCount).toEqual(1);
+    test('add Donut', () => {
+        const underTest = new DonutMaker(0,0);
+        underTest.addDonut(0);
+        expect( underTest.donutCount).toEqual(1);
     });
+
+    test('store autoClicker', () => {
+        const underTest = new DonutMaker(0,0);
+        expect(typeof underTest.autoClickerCount).toMatch('number');
+    });
+
+    test('add autoClicker', ()=> {
+        const underTest = new DonutMaker(1,0);
+        underTest.addAutoClickerCount(0);
+        expect( underTest.autoClickerCount).toEqual(0.1);
+    });
+
+
+    // test ('add Donut', () => {
+    //     const underTest = new DonutMaker(0);
+    //     expect(typeof underTest.donutCount).toEqual(1);
+    // });
+
+    // test ('store AutoClick', ()=> {
+    //     const underTest = new donutCount(1,0);
+    //     underTest.addAutoClicker();
+    //     expect(typeof underTest.donutCount).toMatch('number');
+    // });
 
     
 
-})
+  
+
+    // test('store AutoClick', () => {
+    // const underTest = new AutoClicker(0);
+    // underTest.addAutoClicker();
+    // expect(typeof underTest.autoClicker).toMatch('number');
+    // });
+
+    // test('add AutoClick', () => {
+    // const underTest = new AutoClicker(0);
+    // underTest.addAutoClicker();
+    // expect(underTest.autoClicker).toEqual(1);
+
+    // });
+
+    // test ('Auto Click should take away from donut Counter', () => {
+    // const underTest = new AutoClicker(1);
+    // const underTest = new DonutMaker(1);
+    // underTest.takeAwayDonuts();
+    // expect(uderTest)
+    // });
+
+});
